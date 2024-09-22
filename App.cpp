35b2b2a8552d96042b598a5702dd7802c0708bc3
Smiling <smilingvnmail@gmail.com>
2024-09-22 00:35:51 -0700

    ADD file App.cpp

---
+++ App.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Chuong trinh tinh toan tren 2 so nguyen" << endl;
+    cout << "Starting......" << endl;
+
+    // Code block in here!
+
+
+    return 0;
+    
+}
